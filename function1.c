9364941a8211767b8f5e04955c6494b6c4656431
nGyu <vpdls1511@gmail.com>
2020-05-11 23:23:02 +0900

    Update function1.c

--- function1.c
+++ function1.c
@@ -12,7 +12,7 @@ int main(){
     
     double kg,ms; // 입력을 받을 변수
     
-    printf("질량(kg)과 부피(cm^3) 입력 : "); // 표준 출력 함수
+    printf("질량(kg)과 질량(cm^3) 입력 : "); // 표준 출력 함수
     scanf("%lf %lf",&kg, &ms); // 표준 입력 함수
     
     printf("질량 %.2lf이고 속력 %.2lf의 물체의 운동에너지 :  %.2lf [J]", kg, ms, kineticEnergy(kg, ms)); // 입력받은 값과 연산값 출력
